8307f70b1594f4a9cb71fb55b17e4d09a3882b0a
Ilan Coulon <ilan.coulon@gmail.com>
2015-10-20 18:36:53 +0200

    Ajout des styles dans l'éditeur de l'administration

--- wp-content/themes/moveo/functions.php
+++ wp-content/themes/moveo/functions.php
@@ -160,4 +160,38 @@
 		wp_enqueue_style( 'style', get_stylesheet_uri(), 'bootstrap-theme.min' );
 	}
 	add_action( 'wp_enqueue_scripts', 'moveo_style' );
+
+	// Callback function to insert 'styleselect' into the $buttons array
+	function my_mce_buttons_2( $buttons ) {
+		array_unshift( $buttons, 'styleselect' );
+		return $buttons;
+	}
+	// Register our callback to the appropriate filter
+	add_filter('mce_buttons_2', 'my_mce_buttons_2');
+
+	function my_mce_before_init_insert_formats( $init_array ) {
+		// Define the style_formats array
+		$style_formats = array(
+			// Each array child is a format with it's own settings
+			array(
+				'title' => 'En savoir plus',
+				'block' => 'div',
+				'classes' => 'linkMore',
+				'wrapper' => true
+
+			),
+		);
+		// Insert the array, JSON ENCODED, into 'style_formats'
+		$init_array['style_formats'] = json_encode( $style_formats );
+
+		return $init_array;
+
+	}
+	// Attach callback to 'tiny_mce_before_init'
+	add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
+
+	function my_theme_add_editor_styles() {
+	    add_editor_style( 'editor-style.css' );
+	}
+	add_action( 'admin_init', 'my_theme_add_editor_styles' );
 ?>
