24ad942d02a1aff31eec26a7c3624772364f6907
Ilan Coulon <ilan.coulon@gmail.com>
2015-10-25 12:19:25 +0100

    BUG : style sur la page tampon projets

--- wp-content/themes/moveo/page-templates/page-list-projects.php
+++ wp-content/themes/moveo/page-templates/page-list-projects.php
@@ -16,7 +16,7 @@ $projects = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_da
 <div class="row">
     <?php
     foreach ($projects as $project) {?>
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4" style="min-height:450px">
             <div class="text-center"><a href="<?php echo get_permalink($project->ID); ?>"><?php echo get_the_post_thumbnail( $project->ID, 'thumbnail' ); ?></a></div>
             <h4><?php echo $project->post_title; ?></h4>
                 <?php echo apply_filters('the_excerpt', get_post_field('post_excerpt', $project->ID)); ?>
