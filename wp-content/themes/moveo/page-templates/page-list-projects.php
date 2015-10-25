2567e213830afd8761b04b9e27112296ead31a51
Ilan Coulon <ilan.coulon@gmail.com>
2015-10-25 12:08:09 +0100

    BUG : balise p non fermées sur page tampon projets

--- wp-content/themes/moveo/page-templates/page-list-projects.php
+++ wp-content/themes/moveo/page-templates/page-list-projects.php
@@ -19,9 +19,7 @@ $projects = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_da
         <div class="col-xs-12 col-sm-4">
             <div class="text-center"><a href="<?php echo get_permalink($project->ID); ?>"><?php echo get_the_post_thumbnail( $project->ID, 'thumbnail' ); ?></a></div>
             <h4><?php echo $project->post_title; ?></h4>
-            <p>
                 <?php echo apply_filters('the_excerpt', get_post_field('post_excerpt', $project->ID)); ?>
-            </p>
             <a href="<?php echo get_permalink($project->ID); ?>" class="more">En savoir plus</a>
         </div>
     <?php } ?>
